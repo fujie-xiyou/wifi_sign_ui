76fa2cf9aa07b3d04d5a8044dbe76d9f0d85bd51
fujie <fujie@xiyoulinux.org>
2019-12-08 11:29:41 +0800

    调整缩进风格

--- src/pages/EmptyPage/UserDetail.js
+++ src/pages/EmptyPage/UserDetail.js
@@ -1,48 +1,49 @@
-import React, {Component} from 'react'
-import {host} from './components/Config'
-import {Row, Col} from 'antd'
+import React, { Component } from 'react'
+import { host } from './components/Config'
+import { Row, Col } from 'antd'
 
-import TimeLine from "./components/TimeLine";
+import TimeLine from './components/TimeLine';
 
 class UserDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            result: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+      result: []
+    };
+  }
 
-    componentDidMount() {
-        fetch(host + "/FindSomeday?date=20191208")
-            .then(resp => resp.json())
-            .then(
-                jsonResp => {
-                    if(jsonResp.success){
-                        this.setState({
-                            result: jsonResp.result
-                        })
-                    }
-                },
-                error => {
-                    console.log("error", error)
-                }
-            )
+  componentDidMount() {
+    fetch(host + '/FindSomeday?date=20191208')
+      .then(resp => resp.json())
+      .then(
+        jsonResp => {
+          if (jsonResp.success) {
+            this.setState({
+              result: jsonResp.result
+            })
+          }
+        },
+        error => {
+          console.log('error', error)
+        }
+      )
 
-    }
+  }
 
-    render() {
-        return (
-            this.state.result.map((user) =>
-                <Row key={user.id.toString()} type={"flex"} align={"middle"} gutter={[16, 32]}>
-                    <Col span={1}>{user.name}</Col>
-                    <Col span={20} >
-                        <TimeLine segments={user.onOffLineUtils}/>
-                    </Col>
-                    <Col span={2}>{user.allTimeString}</Col>
-                </Row>
-            )
-        )
-    }
+  render() {
+    return (
+      this.state.result.map((user) =>
+        <Row key={user.id.toString()} type={'flex'} align={'middle'} gutter={[16, 32]}>
+          <Col span={1}>{user.name}</Col>
+          <Col span={20}>
+            <TimeLine segments={user.onOffLineUtils}/>
+          </Col>
+          <Col span={2}>{user.allTimeString}</Col>
+        </Row>
+      )
+    )
+  }
 }
 
 export default UserDetail
