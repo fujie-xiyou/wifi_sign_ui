7bf5ca37462d32d7923e29e3acac012646bfffab
fujie <fujie@xiyoulinux.org>
2019-12-08 21:32:51 +0800

    增加时间轴

--- src/pages/EmptyPage/UserDetail.js
+++ src/pages/EmptyPage/UserDetail.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { host } from './components/Config'
-import { Row, Col } from 'antd'
+import { Row, Col, Icon, DatePicker } from 'antd'
 
 import TimeLine from './components/TimeLine';
 
@@ -9,7 +9,16 @@ class UserDetail extends Component {
     super(props);
     this.state = {
       date: '',
-      result: []
+      result: {
+        userUtils: [
+          {
+            id: '',
+            name: '',
+            onOffLineUtils: [],
+            allTimeString: ''
+          }
+        ]
+      }
     };
   }
 
@@ -30,18 +39,65 @@ class UserDetail extends Component {
       )
 
   }
-
+  getIcon(index){
+    if(index === 0){
+      return <Icon type="crown" theme="twoTone" twoToneColor="#CD7F32"/>;
+    }
+    else if(index === 1){
+      return <Icon type="sketch"/>
+    }
+    else if(index === 2){
+      return <Icon type="trophy" theme="twoTone" twoToneColor="#B5A642"/>;
+    }
+    return null;
+  }
   render() {
+
+    let userUtils = this.state.result.userUtils;
+    const hours = [8, 10, 12, 14, 16, 18, 20, 22];
+
     return (
-      this.state.result.map((user) =>
-        <Row key={user.id.toString()} type={'flex'} align={'middle'} gutter={[16, 32]}>
-          <Col span={1}>{user.name}</Col>
-          <Col span={20}>
-            <TimeLine segments={user.onOffLineUtils}/>
-          </Col>
-          <Col span={2}>{user.allTimeString}</Col>
+      <div>
+        <Row type={'flex'} align={'middle'} >
+          <Col span={2}/>
+          {
+            hours.map((hour) =>
+              <Col style={{textAlign: 'center'}} span={2}>
+                {hour.toString() + ':00'}
+              </Col>
+            )
+          }
         </Row>
-      )
+        <Row type={'flex'} align={'middle'}>
+          <Col span={2}/>
+          {
+            hours.map((hour) =>
+              <Col style={{textAlign: 'center',
+                borderStyle: 'none solid none solid',
+                borderWidth: '1px',
+                borderColor: 'Gainsboro'
+              }} span={2}>
+                |
+              </Col>
+            )
+          }
+        </Row>
+        {
+          userUtils.map((user, index) =>
+            <Row key={user.id.toString()} type={'flex'} align={'middle'} gutter={[16, 32]}>
+              <Col style={{textAlign: 'center'}} span={2}>
+                {this.getIcon(index)}
+                &nbsp;
+                {user.name}
+              </Col>
+              <Col span={17}>
+                <TimeLine segments={user.onOffLineUtils}/>
+              </Col>
+              <Col span={3}>{user.allTimeString}</Col>
+            </Row>
+          )
+        }
+      </div>
     )
   }
 }
